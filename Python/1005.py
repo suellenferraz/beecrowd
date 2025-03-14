d49dca857e282bd31c0b3e928ebb7c7df180a960
suellenferraz <108441500+suellenferraz@users.noreply.github.com>
2025-03-14 12:36:34 +0000

    14/03/2025

---
+++ Python/1005.py
@@ -0,0 +1,7 @@
+# Média 1
+
+A = float(input())
+B = float(input())
+
+Media = (A * 3.5 + B * 7.5) / 11
+print(f"MEDIA = {Media:.5f}")
---
+++ Python/1006.py
@@ -0,0 +1,8 @@
+# Média 2
+
+A = float(input())
+B = float(input())
+C = float(input())
+media = (A*2 + B*3 + C*5)/10.0
+
+print(f'MEDIA = {media:.1f}')
